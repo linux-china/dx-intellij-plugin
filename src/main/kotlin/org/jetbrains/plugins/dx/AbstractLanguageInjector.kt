0e713d8b2c01e24c4c29de3e160adab71227265e
linux_china <libing.chen@gmail.com>
2021-06-26 13:54:45 -0700

    AbstractLanguageInjector

---
+++ src/main/kotlin/org/jetbrains/plugins/dx/AbstractLanguageInjector.kt
@@ -0,0 +1,31 @@
+package org.jetbrains.plugins.dx
+
+import com.intellij.lang.Language
+import com.intellij.lang.javascript.psi.JSReferenceExpression
+import com.intellij.lang.javascript.psi.ecma6.ES6TaggedTemplateExpression
+import com.intellij.lang.javascript.psi.ecma6.JSStringTemplateExpression
+import com.intellij.openapi.util.TextRange
+import com.intellij.psi.InjectedLanguagePlaces
+import com.intellij.psi.LanguageInjector
+import com.intellij.psi.PsiLanguageInjectionHost
+
+
+abstract class AbstractLanguageInjector : LanguageInjector {
+    protected abstract fun getTags(): Array<String>
+    protected abstract fun getInjectionLanguage(): Language?
+
+    override fun getLanguagesToInject(host: PsiLanguageInjectionHost, injectionPlacesRegistrar: InjectedLanguagePlaces) {
+        if (host is JSStringTemplateExpression && getInjectionLanguage() != null) {
+            val taggedTemplate = host.parent
+            if (taggedTemplate is ES6TaggedTemplateExpression) {
+                if (taggedTemplate.tag is JSReferenceExpression) {
+                    val tag = taggedTemplate.tag as JSReferenceExpression
+                    val tagName = tag.text
+                    if (getTags().contains(tagName)) {
+                        injectionPlacesRegistrar.addPlace(getInjectionLanguage()!!, TextRange.create(0, host.getTextLength()), "", "");
+                    }
+                }
+            }
+        }
+    }
+}
